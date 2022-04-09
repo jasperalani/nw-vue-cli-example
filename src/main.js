314e9127f8c1976d233f7f138efeee19c9aa53c4
Jasper Alani <jalani@torpedogroup.com>
2022-04-09 02:34:01 +0100

    add vuex

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 
 import router from './router/index.js';
+import store from './store/index.js';
 
 import applyPrototypes from '@/helpers/applyPrototypes.js';
 
@@ -12,6 +13,7 @@ applyPrototypes(Vue);
 // eslint-disable-next-line no-unused-vars
 const app = new Vue({
   router,
+  store,
 
   render: function (hyperscript) {
     return hyperscript(App);
---
+++ src/store/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue';
+// eslint-disable-next-line import/no-unresolved
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+  },
+  getters: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+});
